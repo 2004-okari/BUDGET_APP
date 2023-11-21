16e9a0fd40fe90408dfb81dc340293d34e8f6a75
okari <nyandika15@gmail.com>
2023-11-21 21:32:13 +0300

    Deploying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,10 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+    # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
